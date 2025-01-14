c3e666f21ab155466c5a5ba49ac5c2dd58c01aa7
TrisMatt20 <tmtmatencio@gmail.com>
2025-01-15 07:52:27 +0800

    Added Classes for PHP

---
+++ classes.php
@@ -0,0 +1,23 @@
+<?php 
+
+class User 
+{
+    public $userID;
+    public $firstName;
+    public $lastName;
+    public $userName;
+    public $userType;
+
+}
+
+class Post {
+    public $postID;
+    public $title;
+    public $description;
+    public $tagID;
+    public $userID;
+    public $ratingID;
+    public $commentID;
+}
+
+?>
